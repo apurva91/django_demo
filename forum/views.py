70ce1aae5d3abd53b127bacb4875e769815415dc
Apurva N. Saraogi <Apurva N. Saraogi>
2017-09-16 08:29:33 +0530

    $1

--- forum/views.py
+++ forum/views.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import ForumPost, PostCategory
 
 # Create your views here.
 
 def Index(request):
 	latest_posts=ForumPost.objects.order_by('-date')[:5]
-	output = '<hr>'.join(["Topic: <a href = './"+str(fp.id)+"'> " + fp.topic + "</a> <br> Posted in: <a href = './category/"+str(fp.category) + "'>" + str(fp.category.name) + "</a> <br> Posted On : " + str(fp.date) 
- 		for fp in latest_posts])
-	return HttpResponse(output)
+	return render(request, 'forum/index.html', {'latest_posts':latest_posts})
 
 def PostDetail(request,post_id):
-	current_post=ForumPost.objects.filter(id=int(post_id))[:1].get()
-	return HttpResponse("Hello You are on a particular post with post id = "+current_post.text)
+
+	current_post = get_object_or_404(ForumPost, pk=post_id)
+	return render(request,'forum/post_detail.html',{'current_post':current_post})
 
 def CategoryIndex(request, category):
-	get_cat_id = PostCategory.objects.order_by('-date')[:5]
-	latest_posts_in_cat=ForumPost.objects.order_by('-date')[:5]
-	output = '<hr>'.join(["Topic: <a href = './"+str(fp.id)+"'> " + fp.topic + "</a> <br> Posted On : " + str(fp.date) 
- 		for fp in latest_posts_in_cat])
-	return HttpResponse(output)
+
+	latest_posts_in_cat=ForumPost.objects.filter(category__url=category).order_by('-date')[:5]
+	if latest_posts_in_cat.count()==0:
+		raise Http404("Category Not Found")
+	return render(request, 'forum/category_index.html', {'latest_posts_in_cat':latest_posts_in_cat})
